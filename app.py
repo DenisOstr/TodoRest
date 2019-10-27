3a31375f85dea418b76b1b0aeb3e16a94cae2c0e
Denis Ostrovsky <denisostrovskis1@gmail.com>
2019-10-27 16:00:21 +0200

    Rewritten RestAPI application

--- app.py
+++ app.py
@@ -1,37 +1,91 @@
-from flask import Flask, jsonify
-from flask import make_response, abort
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
 tasks = [
     {
-        'id': 1,
-        'title': 'first task',
-        'description': 'complete first task',
-        'done': False
+        "id": 1,
+        "title": "Get task",
+        "description": "Create RestAPI for get task",
+        "done": False
     },
     {
-        'id': 2,
-        'title': 'second task',
-        'description': 'complete second task',
-        'done': False
+        "id": 2,
+        "title": "Create task",
+        "description": "Create RestAPI for create task",
+        "done": False
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks/<int:taskId>', methods = ['GET'])
-def getTasks(taskId):
-    task = [task for task in tasks if task['id'] == taskId]
 
-    if len(task) == 0:
-        abort(404)
+class Tasks(Resource):
+    def get(self, id = 0):
+        if id == 0:
+            return tasks, 200
 
-    return jsonify({ 'tasks': tasks[0] })
+        for task in tasks:
+            if task["id"] == id:
+                return task, 200
 
-
-@app.errorhandler(404)
-def notFound(error):
-    return make_response(jsonify( {'error': 'Not Found'} ), 404)
+        return "Task not found", 404
 
 
+    def post(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("title")
+        parser.add_argument("description")
+        params = parser.parse_args()
+
+        for task in tasks:
+            if id == task["id"]:
+                return f"Task with id {id} already exists", 400
+            
+        task = {
+            "id": int(id),
+            "title": params["title"],
+            "description": params["description"],
+            "done": False
+        }
+
+        tasks.append(task)
+
+        return task, 201
+
+
+    def put(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("title")
+        parser.add_argument("description")
+        params = parser.parse_args()
+
+        for task in tasks:
+            if id == task["id"]:
+                task["title"] = params["title"]
+                task["description"] = params["description"]
+
+                return task, 200
+
+        task = {
+            "id": id,
+            "title": params["title"],
+            "description": params["description"],
+            "done": False
+        }
+
+        tasks.append(task)
+
+        return task, 201
+
+
+    def delete(self, id):
+        global tasks
+        tasks = [task for task in tasks if task["id"] != id]
+
+        return f"Task with id {id} is deleted.", 200
+
+
+api.add_resource(Tasks, '/tasks', '/tasks/', '/tasks/<int:id>')
 if __name__ == '__main__':
     app.run(debug=True)
